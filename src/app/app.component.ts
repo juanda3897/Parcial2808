40216272bbb3e04cce4fa7213011a15d62d2f879
Juan Escobar <juand-escobart@unilibre.edu.co>
2019-08-28 21:22:12 -0500

    se muestra la tabla de los planetas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,10 +12,10 @@ export class AppComponent {
   cantidadlunas:string=''
   masa:string=''
   planetas: any=[
-    {nombre:'Pluton', distancia:'10000KM', cantidadlunas:'2', masa:"56000kg"},
-    {nombre:'Mercurio', distancia:'20000KM', cantidadlunas:'1', masa:"76000kg"},
-    {nombre:'Marte', distancia:'30000KM', cantidadlunas:'2', masa:"86000kg"},
-    {nombre:'Venus', distancia:'40000KM', cantidadlunas:'1', masa:"96000kg"}
+    {nombre:'Pluton ', distancia:'10000KM  ', cantidadlunas:'2', masa:"56000kg "},
+    {nombre:'Mercurio ', distancia:'20000KM  ', cantidadlunas:'1', masa:"76000kg "},
+    {nombre:'Marte ', distancia:'30000KM ', cantidadlunas:'2', masa:"86000kg "},
+    {nombre:'Venus ', distancia:'40000KM ', cantidadlunas:'1', masa:"96000kg "}
 ]
 
   title = 'Parcial2808';
