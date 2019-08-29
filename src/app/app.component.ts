bf1ea077cd4e2fca327e738c2b82e2fcc3fff840
Juan Escobar <juand-escobart@unilibre.edu.co>
2019-08-28 21:06:30 -0500

    primer commit, no lista los planetas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  nombre:string=''
+  distancia:string=''
+  cantidadlunas:string=''
+  masa:string=''
+  planetas: any=[
+    {nombre:'Pluton', distancia:'10000KM', cantidadlunas:'2', masa:"56000kg"},
+    {nombre:'Mercurio', distancia:'20000KM', cantidadlunas:'1', masa:"76000kg" },
+    {nombre:'Marte', distancia:'30000KM', cantidadlunas:'2', masa:"86000kg"},
+    {nombre:'Venus', distancia:'40000KM', cantidadlunas:'1', masa:"96000kg"}
+]
+
   title = 'Parcial2808';
 }
