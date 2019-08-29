df673d827b4a35f0e07acda9b3e7a08d775f7ee6
juanescobar <juanescobar@hotmail.com>
2019-08-29 17:57:51 -0500

    Raiz planeta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,11 +11,11 @@ export class AppComponent {
   distancia:string=''
   cantidadlunas:string=''
   masa:string=''
-  planetas: any=[
-    {nombre:'Pluton ', distancia:'10000KM  ', cantidadlunas:'2', masa:"56000kg "},
-    {nombre:'Mercurio ', distancia:'20000KM  ', cantidadlunas:'1', masa:"76000kg "},
-    {nombre:'Marte ', distancia:'30000KM ', cantidadlunas:'2', masa:"86000kg "},
-    {nombre:'Venus ', distancia:'40000KM ', cantidadlunas:'1', masa:"96000kg "}
+    planetas: any=[
+    {nombre:'Pluton ', distancia:'10000KM  ', cantidadlunas:'2', masa:"56000kg ", anillos:1},
+    {nombre:'Mercurio ', distancia:'20000KM  ', cantidadlunas:'1', masa:"76000kg ", anillos:2 },
+    {nombre:'Marte ', distancia:'30000KM ', cantidadlunas:'2', masa:"86000kg ", anillos:1},
+    {nombre:'Venus ', distancia:'40000KM ', cantidadlunas:'1', masa:"96000kg ", anillos:1}
 ]
 
   title = 'Parcial2808';
